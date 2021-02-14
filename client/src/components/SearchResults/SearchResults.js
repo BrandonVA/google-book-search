35ae9300bf1dda409810f64cb2810f914d05e717
BrandonVA <codebva@gmail.com>
2021-02-13 19:08:49 -0800

    Set up save book function

--- client/src/components/SearchResults/SearchResults.js
+++ client/src/components/SearchResults/SearchResults.js
@@ -1,29 +1,72 @@
+// import { Button } from "bootstrap";
 import React from "react";
+import { Row, Col, Button } from "react-bootstrap";
+import { Markup } from "interweave";
+import API from "../../utils/API";
 
 const SearchResults = ({ books }) => {
+  const saveBook = (book) => {
+    const bookToSave = {
+      title: book.volumeInfo.title,
+      author: book.volumeInfo.authors,
+      description: book.volumeInfo.description
+        ? book.volumeInfo.description
+        : book.searchInfo.textSnippet,
+      link: book.volumeInfo.infoLink,
+      image: book.volumeInfo.imageLinks.thumbnail,
+    };
+    API.saveBook(bookToSave);
+    console.log(bookToSave);
+  };
   return (
-    <div>
+    <Row className="m-3 p-3">
       {books.map((book) => (
-        <div key={book.id}>
-          <h4>{book.volumeInfo.title}</h4>
-          <h6>
-            {book.volumeInfo.authors
-              ? book.volumeInfo.authors.map((name) => (
-                  <strong> {name}, </strong>
-                ))
-              : "No Authors"}
-          </h6>
-          <div>
-            <div></div>
-            <div></div>
+        <Col xl={12} key={book.id}>
+          <Row>
+            <Col xl={8}>
+              <h4>{book.volumeInfo.title}</h4>
+              <h6>
+                Written By:
+                {book.volumeInfo.authors
+                  ? book.volumeInfo.authors.map((name, i) => (
+                      <strong key={i}>
+                        {" "}
+                        {`${name} ${
+                          book.volumeInfo.authors.length > 1 &&
+                          i + 1 !== book.volumeInfo.authors.length
+                            ? ","
+                            : ""
+                        }`}{" "}
+                      </strong>
+                    ))
+                  : "No Authors"}
+              </h6>
+            </Col>
+            <Col xl={4}>
+              {" "}
+              <div style={{ display: "flex", justifyContent: "flex-end" }}>
+                <Button className="m-1" onClick={saveBook}>
+                  View
+                </Button>{" "}
+                <Button className="m-1" onClick={() => saveBook(book)}>
+                  Save
+                </Button>
+              </div>
+            </Col>
+          </Row>
 
-            {book.volumeInfo ? book.volumeInfo.description : book.subtitle}
+          <div>
+            {book.volumeInfo.description ? (
+              book.volumeInfo.description
+            ) : (
+              <Markup content={book.searchInfo.textSnippet} />
+            )}
           </div>
 
           <hr />
-        </div>
+        </Col>
       ))}
-    </div>
+    </Row>
   );
 };
 
