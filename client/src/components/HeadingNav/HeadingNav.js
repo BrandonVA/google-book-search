435b861af4eb78ab745b702f2704d72ac63582c0
BrandonVA <codebva@gmail.com>
2021-02-22 15:36:39 -0800

    updated heading nav

--- client/src/components/HeadingNav/HeadingNav.js
+++ client/src/components/HeadingNav/HeadingNav.js
@@ -1,19 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Nav } from "react-bootstrap";
+import "./nav.css";
 
 const HeadingNav = () => {
+  const [location, setLocation] = useState("");
+  useEffect(() => {
+    setLocation(window.location.pathname);
+  }, [location]);
+
+  const setClass = (url) => (url === location ? "active nav-link" : "nav-link");
+
   return (
     <Nav defaultActiveKey="/" as="ul">
-      <Nav.Item as="li">
-        <Nav.Link href="/">Google Books</Nav.Link>
-      </Nav.Item>
-      <Nav.Item as="li">
-        <Link to="/">Search</Link>
-      </Nav.Item>
-      <Nav.Item as="li">
-        <Link to="/saved">Saved</Link>
-      </Nav.Item>
+      <Link
+        to="/"
+        onClick={() => {
+          setLocation("/");
+        }}
+        className={"nav-link"}
+      >
+        Google Books
+      </Link>
+      <Link
+        to="/"
+        onClick={() => {
+          setLocation("/");
+        }}
+        className={setClass("/")}
+      >
+        Search
+      </Link>
+      <Link
+        to="/saved"
+        onClick={() => {
+          setLocation("/saved");
+        }}
+        className={setClass("/saved")}
+      >
+        Saved
+      </Link>
     </Nav>
   );
 };
